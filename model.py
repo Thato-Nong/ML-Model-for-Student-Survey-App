343b8c3c9e3ac0e34358369f2cccecf38d1a0180
frank <netshiavhafrank@gmail.com>
2023-11-15 01:46:02 +0200

    added something cool

--- model.py
+++ model.py
@@ -1,9 +1,52 @@
 # Step 1: Data Collection and Preparation
+import textblob as TextBlob
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import csv
+
+
+def analyze_sentiment(sentences):
+    sentiments = []
+
+    for sentence in sentences:
+        # Create a TextBlob object for sentiment analysis
+        blob = TextBlob.TextBlob(sentence)
+
+        # Get the sentiment polarity (ranges from -1 to 1)
+        sentiment_polarity = blob.sentiment.polarity
+
+        # Classify sentiment based on polarity
+        if sentiment_polarity > 0:
+            sentiments.append('Positive')
+        elif sentiment_polarity < 0:
+            sentiments.append('Negative')
+        else:
+            sentiments.append('Neutral')
+
+    return sentiments
+
+def read_csv(file_path):
+    with open(file_path, 'r') as csvfile:
+        # Create a CSV reader object
+        csvreader = csv.reader(csvfile)
+
+        # Read and print each line in a readable format
+        # for row in csvreader:
+        #     print(', '.join(row))
+        #
+        #
+
+        rows = []
+        for row in csvreader:
+            x=', '.join(row)
+            print(x)
+            rows.append(x)
+
+    return rows
+
 
 # Load your dataset with student responses
 data = pd.read_csv('large_student_responses.csv')
@@ -33,3 +76,14 @@ print(f'Text Model Accuracy: {accuracy_text}')
 
 # Step 4: Model Deployment and Interpretation
 # ... (Add deployment and interpretation steps here)
+# Replace 'your_data.csv' with the actual file path
+file_path = 'input.csv'
+print(analyze_sentiment(read_csv(file_path)))
+
+
+
+
+
+
+
+
